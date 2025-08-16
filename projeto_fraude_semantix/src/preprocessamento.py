a0237d6186573e2082f5fa94515042dbd0f25d81
Vitor Nobre <nobre.vitor@hotmail.com>
2025-08-15 21:43:30 -0300

    Mudanças preprocessamento

--- projeto_fraude_semantix/src/preprocessamento.py
+++ projeto_fraude_semantix/src/preprocessamento.py
@@ -1,10 +1,6 @@
 import pandas as pd 
 from sklearn.preprocessing import StandardScaler
 
-def carregar_dados(caminho: str) -> pd.DataFrame:
-    """Carrega o dataset a partir de um arquivo CSV."""
-    return pd.read_csv(caminho)
-
 def limpar_dados(df: pd.DataFrame) -> pd.DataFrame:
     """Remove valores ausentes e possíveis duplicatas."""
     df = df.dropna()
